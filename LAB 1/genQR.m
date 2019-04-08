b1c7e717f7286d3265603b1a68c140c6ea6af732
Denis <denis.suozzi@mail.polimi.it>
2019-04-08 09:43:19 +0200

    comment modified in genQR.m

--- LAB 1/genQR.m
+++ LAB 1/genQR.m
@@ -12,7 +12,7 @@ end
 Q = [];
 R = [];
 L = [];
-%Matrix L if right-multiplied to the input matrix selects a set of columns
+%Matrix L if right-multiplied to the input matrix selects a set of maximal columns
 %that is linearly indipendent. If right-multiplied to the R matrix makes it
 %square and upper triangular.
 
